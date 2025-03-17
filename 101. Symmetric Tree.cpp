a4b9a04b7a6acadbc4f456bf1427a061df889be2
Dorj Enkhbold <e.dorj1016@gmail.com>
2025-03-17 16:17:05 +0800

    2025-3-17

--- 101. Symmetric Tree.cpp
+++ 101. Symmetric Tree.cpp
@@ -11,26 +11,26 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-// class Solution
-// {
-// public:
-//     bool isSymmetric(TreeNode *root)
-//     {
-//         return isSymmetricHelper(root->left, roo);
-//     }
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode *root)
+    {
+        return isSymmetricHelper(root->left, root->right);
+    }
 
-// private:
-//     bool isSymmetricHelper(TreeNode *root1, TreeNode *root2)
-//     {
-//         if (!root1 && !root2)
-//             return true;
+private:
+    bool isSymmetricHelper(TreeNode *root1, TreeNode *root2)
+    {
+        if (!root1 && !root2)
+            return true;
 
-//         if (!root1 || !root2)
-//             return false;
+        if (!root1 || !root2)
+            return false;
 
-//         if (root1->val != root2->val)
-//             return false;
+        if (root1->val != root2->val)
+            return false;
 
-//         return isSymmetricHelper(root1->left, root2->right) && isSymmetricHelper(root1->right, root2->left);
-//     }
-// };
+        return isSymmetricHelper(root1->left, root2->right) && isSymmetricHelper(root1->right, root2->left);
+    }
+};
