8326eacac4961dff516aebc49d134fc51948c0e6
e.dorj1016 <e.dorj1016@gmail.com>
2025-03-26 11:32:09 +0800

    2025-03-26

---
+++ 168. Excel Sheet Column Title.cpp
@@ -0,0 +1,26 @@
+#include "./lib/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    string convertToTitle(int columnNumber)
+    {
+        return "qwe";
+    }
+};
+
+int main()
+{
+    Solution solution;
+    string result;
+    result = solution.convertToTitle(1);
+    cout << result << endl;
+
+    result = solution.convertToTitle(28);
+    cout << result << endl;
+
+    result = solution.convertToTitle(701);
+    cout << result << endl;
+    return 0;
+}
