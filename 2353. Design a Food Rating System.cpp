431bd1b25d3a90c69a72f8aab2b1d9082b2a8b48
Dorj <e.dorj1016@gmail.com>
2025-09-17 17:20:59 +0800

    2025-09-17

---
+++ 2353. Design a Food Rating System.cpp
@@ -0,0 +1,23 @@
+#include "./lib/stdc++.h"
+using namespace std;
+
+class FoodRatings {
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        
+    }
+    
+    void changeRating(string food, int newRating) {
+        
+    }
+    
+    string highestRated(string cuisine) {
+        
+    }
+};
+
+int main(){
+    FoodRatings solution;
+    
+    return 0;
+}
